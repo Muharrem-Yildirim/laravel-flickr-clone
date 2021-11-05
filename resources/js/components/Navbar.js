74995680fecf106a9d0874a6a6d66d1350a7fb47
Muharrem Yıldırım <muharremyildirim303@gmail.com>
2021-11-05 18:19:15 +0300

    Added search field to navbar

--- resources/js/components/Navbar.js
+++ resources/js/components/Navbar.js
@@ -5,14 +5,57 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-// // import MenuIcon from "@mui/icons-material/Menu";
-// import MenuIcon from "@material-ui/icons/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Grid } from "@mui/material";
 import { Link } from "react-router-dom";
 import UploadIcon from "@mui/icons-material/Upload";
 import { useDispatch } from "react-redux";
 import { openModal } from "../actions/modalActions";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import TextField from "@mui/material/TextField";
+import { outlinedInputClasses } from "@mui/material/OutlinedInput";
+import { inputLabelClasses } from "@mui/material/InputLabel";
+import SearchIcon from "@mui/icons-material/Search";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import styled from "styled-components";
+
+const WhiteBorderTextField = styled(TextField)({
+    [`& .${outlinedInputClasses.root} .${outlinedInputClasses.notchedOutline}`]:
+        {
+            // borderColor: "white",
+        },
+    [`&:hover .${outlinedInputClasses.root} .${outlinedInputClasses.notchedOutline}`]:
+        {
+            borderColor: "white",
+        },
+    [`& .${outlinedInputClasses.root}.${outlinedInputClasses.focused} .${outlinedInputClasses.notchedOutline}`]:
+        {
+            borderColor: "white",
+        },
+    [`& .${outlinedInputClasses.input}`]: {
+        color: "white",
+    },
+    [`&:hover .${outlinedInputClasses.input}`]: {
+        color: "white",
+    },
+    [`& .${outlinedInputClasses.root}.${outlinedInputClasses.focused} .${outlinedInputClasses.input}`]:
+        {
+            color: "white",
+        },
+    [`& .${inputLabelClasses.outlined}`]: {
+        color: "white",
+    },
+    [`&:hover .${inputLabelClasses.outlined}`]: {
+        color: "white",
+    },
+    [`& .${inputLabelClasses.outlined}.${inputLabelClasses.focused}`]: {
+        color: "white",
+    },
+});
 
 const Logo = () => (
     <Link to="/" style={{ textDecoration: "none", color: "white" }}>
@@ -50,17 +93,27 @@ export default function Navbar() {
                         marginRight: "auto",
                     }}
                 >
-                    {/* <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton> */}
                     <Logo />
-                    {/* <Button color="inherit">Explore</Button> */}
+
+                    <Box sx={{ flexGrow: 1 }} />
+
+                    <WhiteBorderTextField
+                        value={""}
+                        size="small"
+                        style={{ borderColor: "white" }}
+                        variant="outlined"
+                        disabled={true}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton edge="end">
+                                        <SearchIcon />
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                        }}
+                        label="Search"
+                    />
 
                     <Box sx={{ flexGrow: 1 }} />
                     <IconButton
